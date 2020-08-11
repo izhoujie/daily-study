6da97d873c0aeba9fc6859574d92ce43abcc72a8
ZhouJie <macula@vip.qq.com>
2020-08-11 15:06:50 +0800

    删除旧项目代码

--- Guice/src/main/java/com/izhoujie/Guice/imp/HelloworldSubImpl.java
+++
@@ -1,9 +0,0 @@
-package com.izhoujie.Guice.imp;
-
-public class HelloworldSubImpl extends HelloWorldImpl {
-	@Override
-	public String sayHello() {
-		// TODO Auto-generated method stub
-		return "Sub Hello!";
-	}
-}
